d5d0620b31ed2f58d161737ec6e1acfa8d625244
xinxu <azzlzzxz@gmail.com>
2022-07-22 09:21:12 +0800

    feat 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: xinxu
+ * @Date: 2022-07-21 14:12:01
+ * @LastEditors: xinxu
+ * @LastEditTime: 2022-07-21 14:50:12
+ * @FilePath: /react-vite/src/App.tsx
+ */
+import {Header} from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: xinxu
+ * @Date: 2022-07-21 14:47:52
+ * @LastEditors: xinxu
+ * @LastEditTime: 2022-07-21 14:48:17
+ * @FilePath: /react-vite/src/components/Header/index.tsx
+ */
+import './index.scss';
+
+export function Header() {
+    return <p className='header'>Header</p>
+}
