64115c666912a63fe21e0726e76e51ad23193be0
xinxu <azzlzzxz@gmail.com>
2022-07-24 22:55:35 +0800

    feat Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,11 @@
  * @Author: xinxu
  * @Date: 2022-07-21 14:47:52
  * @LastEditors: xinxu
- * @LastEditTime: 2022-07-21 14:48:17
+ * @LastEditTime: 2022-07-24 22:55:21
  * @FilePath: /react-vite/src/components/Header/index.tsx
  */
 import './index.scss';
 
 export function Header() {
-    return <p className='header'>Header</p>
+    return <p className='header'>This is Header</p>
 }
