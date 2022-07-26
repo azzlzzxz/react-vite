19bc2dfc50b43171709bb408b0caf47e3499138c
xinxu <azzlzzxz@gmail.com>
2022-07-26 19:36:05 +0800

    feat: text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: xinxu
  * @Date: 2022-07-21 14:12:01
  * @LastEditors: xinxu
- * @LastEditTime: 2022-07-25 10:57:11
+ * @LastEditTime: 2022-07-26 19:35:40
  * @FilePath: /react-vite/src/App.tsx
  */
 import { Header } from './components/Header';
@@ -10,7 +10,7 @@ import { Header } from './components/Header';
 function App() {
   return (
     <div>
-      <p className="bg-red-400">Hello Vite + React!</p>
+      <p className="bg-red-400">Hello Vite + React!!</p>
       <Header />
     </div>
   );
